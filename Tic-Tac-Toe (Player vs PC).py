de1ed0c065d2e3d9a74a3211f2c008a707cd7865
Vena07 <vasiknosek@email.cz>
2024-02-12 00:42:07 +0100

    Tic-Tac-Toe.ver2(player vs PC)

--- Tic-Tac-Toe (Player vs PC).py
+++ Tic-Tac-Toe (Player vs PC).py
@@ -14,7 +14,7 @@ pole7 = "7"
 pole8 = "8"
 pole9 = "9"
 
-
+vyhodnoceno = True
 konec = False
 vyhra = False
 
@@ -131,57 +131,96 @@ def SanceVyhry():
                             elif pole9 == pole3 == "O" and pole6 == "6":
                                 pole6 = "O"
                                 vyhra = True
-                       
+                            else:
+                                if pole4 == pole5 == "O" and pole6 == "6":
+                                    pole6 = "O"
+                                elif pole6 == pole5 == "O" and pole4 == "4":
+                                    pole9 = "O"
+                                elif pole4 == pole6 == "O" and pole5 == "5":
+                                    pole5 = "O"                        
+
 def SanceBloku():
-        global  pole1, pole2,pole3 ,pole4,pole5,pole6,pole7,pole8,pole9,vyhra  
+        global  pole1, pole2,pole3 ,pole4,pole5,pole6,pole7,pole8,pole9,vyhodnoceno  
+        vyhodnoceno = True
         if pole1 == pole2 == "X" and pole3 == "3":
             pole3 = "O"
+            vyhodnoceno = False
         elif pole2 == pole3 == "X" and pole1 == "1":
             pole1 = "O"
+            vyhodnoceno = False
         elif pole1 == pole3 == "X" and pole2 == "2":
             pole2 = "O"
+            vyhodnoceno = False
         else:
             if pole7 == pole8 == "X" and pole9 == "9":
                 pole9 = "O"
+                vyhodnoceno = False
             elif pole8 == pole9 == "X" and pole7 == "7":
                 pole7 = "O"
+                vyhodnoceno = False
             elif pole7 == pole9 == "X" and pole8 == "8":
                 pole8 = "O"
+                vyhodnoceno = False
             else:
                 if pole1 == pole5 == "X" and pole9 == "9":
                     pole9 = "O"
+                    vyhodnoceno = False
                 elif pole5 == pole9 == "X" and pole1 == "1":
                     pole1 = "O"
+                    vyhodnoceno = False
                 elif pole1 == pole9 == "X" and pole5 == "5":
                     pole5 = "O"
+                    vyhodnoceno = False
                 else:
                     if pole3 == pole5 == "X" and pole7 == "7":
                         pole7 = "O"
+                        vyhodnoceno = False
                     elif pole5 == pole7 == "X" and pole3 == "3":
                         pole3 = "O"
+                        vyhodnoceno = False
                     elif pole3 == pole7 == "X" and pole5 == "5":
                         pole5 = "O"
+                        vyhodnoceno = False
                     else:
                         if pole7 == pole4 == "X" and pole1 == "1":
                             pole1 = "O"
+                            vyhodnoceno = False
                         elif pole4 == pole1 == "X" and pole7 == "7":
                             pole7 = "O"
+                            vyhodnoceno = False
                         elif pole7 == pole1 == "X" and pole4 == "4":
-                            pole4 = "O"    
-                        else:      
+                            pole4 = "O"
+                            vyhodnoceno = False
+                        else:
                             if pole8 == pole5 == "X" and pole2 == "2":
                                 pole2 = "O"
+                                vyhodnoceno = False
                             elif pole5 == pole2 == "X" and pole8 == "8":
                                 pole8 = "O"
+                                vyhodnoceno = False
                             elif pole8 == pole2 == "X" and pole5 == "5":
                                 pole5 = "O"
+                                vyhodnoceno = False
                             else:
                                 if pole9 == pole6 == "X" and pole3 == "3":
                                     pole3 = "O"
+                                    vyhodnoceno = False
                                 elif pole6 == pole3 == "X" and pole9 == "9":
                                     pole9 = "O"
+                                    vyhodnoceno = False
                                 elif pole9 == pole3 == "X" and pole6 == "6":
-                                    pole6 = "O"                 
+                                    pole6 = "O"
+                                    vyhodnoceno = False
+                                else:
+                                    if pole4 == pole5 == "X" and pole6 == "6":
+                                        pole6 = "O"
+                                        vyhodnoceno = False
+                                    elif pole6 == pole5 == "X" and pole4 == "4":
+                                        pole9 = "O"
+                                        vyhodnoceno = False
+                                    elif pole4 == pole6 == "X" and pole5 == "5":
+                                        pole5 = "O"
+                                        vyhodnoceno = False 
 
 def Pole():
     for i in range(2):
@@ -221,6 +260,13 @@ def vyhodnoceni():
     if pole1 == pole2 == pole3 or pole4 == pole5 == pole6 or pole7 == pole8 == pole9 or pole1 == pole4 == pole7 or pole2 == pole5 == pole8 or pole3 == pole6 == pole9 or pole7 == pole5 == pole3 or pole9 == pole5 == pole1:
         vyhra = True
 
+def Remiza():
+    global vyhra
+    if (pole1 == "X" or pole1 == "O") and (pole2 == "X" or pole2 == "O") and (pole3 == "X" or pole3 == "O") and (pole4 == "X" or pole4 == "O") and (pole5 == "X" or pole5 == "O") and (pole6 == "X" or pole6 == "O") and (pole7 == "X" or pole7 == "O") and (pole8 == "X" or pole8 == "O") and (pole9 == "X" or pole9 == "O"):
+        Pole()
+        print("Konec hry")
+        vyhra = True   
+    
 kolo=0
 
 while konec == False:
@@ -235,6 +281,7 @@ while konec == False:
             kontrola = False
     VyberPole()
     vyhodnoceni()
+    Remiza()
     if vyhra == True:
         Pole()
         print("Konec hry")
@@ -248,13 +295,15 @@ while konec == False:
     if kolo==0:
         VyberPrvnihoKola()
         kolo+=1
-    
-    if kolo >0:
+    else:
         SanceVyhry()
 
         if vyhra==False:
             SanceBloku()
-
+            if vyhodnoceno == True:
+                VyberPrvnihoKola()
+    
+    Remiza()
     if vyhra == True:
         Pole()
         print("Konec hry")
